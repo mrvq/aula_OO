9edd7403ce4265c78e321e833759930c70f604fc
Massu <massu.queirantes@yahoo.com.br>
2026-06-07 15:33:23 -0300

    feat:"desafio_16_guanabara"

---
+++ desafios/desafio16.py
@@ -0,0 +1,15 @@
+class Funcionario(self, nome, setor, cargo)
+    self.nome
+    self.setor
+    self.cago
+    
+    def Apresentacao(nome, setor, cargo)
+        print(f"O nome do funcionario é {}, o setor que ele trabalhar é {}, e o cargo dele é {}")
+
+    c1 = Funcionario("Maria", "TI", "Programador")
+    print(c1.apresentacao)
+    
+    c2 = Funcionario("Pedro", "Administrativo", "Diretoria")
+    print(c2.apresentacao)
+    
+    
---
+++ rich/rich001.py
@@ -0,0 +1,4 @@
+from rich import print
+
+print("Ola, [red]Mundo[/red]! :earth_americas:")
+print("Ola, [bold blue]Pequeno Gafanhoto[/] :vulcan_salute:")
